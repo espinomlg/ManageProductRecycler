ab7bd1676da63a62da919a483eaeab76af304ee2
Andres Espino Lopez <espinomlg@gmail.com>
2016-10-23 19:18:39 +0200

    actualizado

--- app/src/main/java/com/example/espino/manageproduct/AddProduct_Activity.java
+++ app/src/main/java/com/example/espino/manageproduct/AddProduct_Activity.java
@@ -1,10 +1,59 @@
 package com.example.espino.manageproduct;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
-/**
- * Created by espino on 23/10/16.
- */
+
 
 public class AddProduct_Activity extends AppCompatActivity {
+
+    private EditText mName,mDescription,mDossage,mBrand,mPrice,mStock,mImage;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_product);
+
+        mName = (EditText) findViewById(R.id.nameEdt);
+        mDescription = (EditText) findViewById(R.id.editDescription);
+        mDossage = (EditText) findViewById(R.id.dossageEdt);
+        mBrand = (EditText) findViewById(R.id.brandEdt);
+        mPrice = (EditText) findViewById(R.id.editPrice);
+        mStock = (EditText) findViewById(R.id.stockEdt);
+        mImage = (EditText) findViewById(R.id.imageEdt);
+
+
+    }
+
+    public void addProduct(View v){
+
+       /* Product p = new Product(mName.getText().toString(),
+                mDescription.getText().toString(),
+                mDossage.getText().toString(),
+                mBrand.getText().toString(),
+                Double.parseDouble(mPrice.getText().toString()),
+                Integer.parseInt(mStock.getText().toString()),
+                R.drawable.medicamento);
+*/
+        Bundle b = new Bundle();
+        b.putString("name",mName.getText().toString());
+        b.putString("description",mDescription.getText().toString());
+        b.putString("dossage",mDossage.getText().toString());
+        b.putString("brand",mBrand.getText().toString());
+        b.putString("price",mPrice.getText().toString());
+        b.putString("stock",mStock.getText().toString());
+        b.putString("image",mImage.getText().toString());
+
+        //((ProductApplication)getApplicationContext()).saveProduct(p);
+
+        Intent returnIntent = new Intent();
+        returnIntent.putExtras(b);
+        setResult(Activity.RESULT_OK,returnIntent);
+        finish();
+    }
 }
--- app/src/main/java/com/example/espino/manageproduct/ListProduct_activity.java
+++ app/src/main/java/com/example/espino/manageproduct/ListProduct_activity.java
@@ -1,26 +1,46 @@
 package com.example.espino.manageproduct;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import com.example.espino.manageproduct.Modelo.Product;
 
 
 public class ListProduct_activity extends ListActivity {
 
-    private ProductAdapterA adapter;
+    private ProductAdapterB adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_product_activity);
 
-        adapter = new ProductAdapterA(this);
+        adapter = new ProductAdapterB(this);
         getListView().setAdapter(adapter);
     }
 
     public void onClick(View v){
-
+        startActivityForResult(new Intent(ListProduct_activity.this,AddProduct_Activity.class),0);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        Bundle b = new Bundle(data.getExtras());
+        Product p = new Product(b.getString("name"),
+                b.getString("description"),
+                b.getString("dossage"),
+                b.getString("brand"),
+                Double.parseDouble(b.getString("price")),
+                Integer.parseInt(b.getString("stock")),
+                R.drawable.medicamento);
+
+
+        adapter.add(p);
+
+
+    }
 }
