9aed7ee0fd9c8e4706b4efb08c9d0fe274c187fe
Andres Espino Lopez <espinomlg@gmail.com>
2016-10-23 19:07:42 +0200

    actualizado

---
+++ app/src/main/java/com/example/espino/manageproduct/AddProduct_Activity.java
@@ -0,0 +1,10 @@
+package com.example.espino.manageproduct;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by espino on 23/10/16.
+ */
+
+public class AddProduct_Activity extends AppCompatActivity {
+}
